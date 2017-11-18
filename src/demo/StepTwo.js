039dea91b2f5f45fb03897d32a9326550afd9b4f
guidoec <guidoceraso@gmail.com>
2017-11-18 23:14:59 +0100

    Continue step two

--- src/demo/StepTwo.js
+++ src/demo/StepTwo.js
@@ -2,10 +2,23 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class StepTwo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.props.canContinue({value: true});
+    }
+
     render() {
         return (
             <div style={{padding: '2rem 3rem', textAlign: 'left'}}>
-                TEST
+                <div className="btn btn-warning btn-block" onClick={this.handleClick}>
+                    Click to enable finish button
+                </div>
             </div>
         )
     }
